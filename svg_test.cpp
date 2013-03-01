3001015504dbfc040d6c40cfe7ca903f68399ac1
Ming <m@ufoym.com>
2013-03-01 15:13:17 +0800

    + zoom in/out, adaptive size

--- svg_test.cpp
+++ svg_test.cpp
@@ -10,19 +10,12 @@
 #include "ctrl/agg_slider_ctrl.h"
 #include "agg_svg_parser.h"
 
-//#include "agg_gamma_lut.h"
-
 enum { flip_y = false };
 
 
 class the_application : public agg::platform_support
 {
     agg::svg::path_renderer m_path;
-
-    agg::slider_ctrl<agg::rgba8> m_expand;
-    agg::slider_ctrl<agg::rgba8> m_gamma;
-    agg::slider_ctrl<agg::rgba8> m_scale;
-    agg::slider_ctrl<agg::rgba8> m_rotate;
 
     double m_min_x;
     double m_min_y;
@@ -35,15 +28,13 @@ class the_application : public agg::platform_support
     double m_dy;
     bool   m_drag_flag;
 
+	double m_scale;
+
 public:
 
     the_application(agg::pix_format_e format, bool flip_y) :
         agg::platform_support(format, flip_y),
         m_path(),
-        m_expand(5,     5,    256-5, 11,    !flip_y),
-        m_gamma (5,     5+15, 256-5, 11+15, !flip_y),
-        m_scale (256+5, 5,    512-5, 11,    !flip_y),
-        m_rotate(256+5, 5+15, 512-5, 11+15, !flip_y),
         m_min_x(0.0),
         m_min_y(0.0),
         m_max_x(0.0),
@@ -52,28 +43,9 @@ public:
         m_y(0.0),
         m_dx(0.0),
         m_dy(0.0),
-        m_drag_flag(false)
+        m_drag_flag(false),
+		m_scale(1.0)
     {
-        add_ctrl(m_expand);
-        add_ctrl(m_gamma);
-        add_ctrl(m_scale);
-        add_ctrl(m_rotate);
-
-        m_expand.label("Expand=%3.2f");
-        m_expand.range(-1, 1.2);
-        m_expand.value(0.0);
-
-        m_gamma.label("Gamma=%3.2f");
-        m_gamma.range(0.0, 3.0);
-        m_gamma.value(1.0);
-
-        m_scale.label("Scale=%3.2f");
-        m_scale.range(0.2, 10.0);
-        m_scale.value(1.0);
-
-        m_rotate.label("Rotate=%3.2f");
-        m_rotate.range(-180.0, 180.0);
-        m_rotate.value(0.0);
     }
 
     void parse_svg(const char* fname)
@@ -86,7 +58,14 @@ public:
     }
 
     virtual void on_resize(int cx, int cy)
-    {
+	{
+		double w = m_max_x-m_min_x;
+		double h = m_max_y-m_min_y;
+		double scale_x = (cx-50) / w;
+		double scale_y = (cy-50) / h;
+		m_scale = (scale_x < scale_y) ? scale_x : scale_y;
+		m_x = cx / 2;
+		m_y = (cy-50) / 2;
     }
 
     virtual void on_draw()
@@ -105,25 +84,15 @@ public:
         agg::scanline_p8 sl;
         agg::trans_affine mtx;
 
-        ras.gamma(agg::gamma_power(m_gamma.value()));
         mtx *= agg::trans_affine_translation((m_min_x + m_max_x) * -0.5, (m_min_y + m_max_y) * -0.5);
-        mtx *= agg::trans_affine_scaling(m_scale.value());
-        mtx *= agg::trans_affine_rotation(agg::deg2rad(m_rotate.value()));
+        mtx *= agg::trans_affine_scaling(m_scale);
         mtx *= agg::trans_affine_translation((m_min_x + m_max_x) * 0.5 + m_x, (m_min_y + m_max_y) * 0.5 + m_y + 30);
         
-        m_path.expand(m_expand.value());
         start_timer();
         m_path.render(ras, sl, ren, mtx, rb.clip_box(), 1.0);
         double tm = elapsed_time();
         unsigned vertex_count = m_path.vertex_count();
-
-        ras.gamma(agg::gamma_none());
-        agg::render_ctrl(ras, sl, rb, m_expand);
-        agg::render_ctrl(ras, sl, rb, m_gamma);
-        agg::render_ctrl(ras, sl, rb, m_scale);
-        agg::render_ctrl(ras, sl, rb, m_rotate);
-
-
+		
         char buf[128]; 
         agg::gsv_text t;
         t.size(10.0);
@@ -134,29 +103,13 @@ public:
 
         sprintf(buf, "Vertices=%d Time=%.3f ms", vertex_count, tm);
 
-        t.start_point(10.0, 40.0);
+        t.start_point(10.0, 20.0);
         t.text(buf);
 
         ras.add_path(pt);
         ren.color(agg::rgba(0,0,0));
         agg::render_scanlines(ras, sl, ren);
 
-
-        //agg::gamma_lut<> gl(m_gamma.value());
-        //unsigned x, y;
-        //unsigned w = unsigned(width());
-        //unsigned h = unsigned(height());
-        //for(y = 0; y < h; y++)
-        //{
-        //    for(x = 0; x < w; x++)
-        //    {
-        //        agg::rgba8 c = rb.pixel(x, y);
-        //        c.r = gl.inv(c.r);
-        //        c.g = gl.inv(c.g);
-        //        c.b = gl.inv(c.b);
-        //        rb.copy_pixel(x, y, c);
-        //    }
-        //}
     }
 
     virtual void on_mouse_button_down(int x, int y, unsigned flags)
@@ -188,28 +141,16 @@ public:
 
     virtual void on_key(int x, int y, unsigned key, unsigned flags)
     {
-        if(key == ' ')
-        {
-
-            agg::trans_affine mtx;
-            mtx *= agg::trans_affine_translation((m_min_x + m_max_x) * -0.5, (m_min_y + m_max_y) * -0.5);
-            mtx *= agg::trans_affine_scaling(m_scale.value());
-            mtx *= agg::trans_affine_rotation(agg::deg2rad(m_rotate.value()));
-            mtx *= agg::trans_affine_translation((m_min_x + m_max_x) * 0.5, (m_min_y + m_max_y) * 0.5);
-            mtx *= agg::trans_affine_translation(m_x, m_y);
-
-            double m[6];
-            mtx.store_to(m);
-
-            char buf[128];
-            sprintf(buf, "%3.3f, %3.3f, %3.3f, %3.3f, %3.3f, %3.3f",
-                         m[0], m[1], m[2], m[3], m[4], m[5]);
-
-            message(buf);
-            FILE* fd = fopen(full_file_name("transform.txt"), "a");
-            fprintf(fd, "%s\n", buf);
-            fclose(fd);
-        }
+        if(key == 'a')
+		{
+			m_scale *= 1.2;
+			force_redraw();
+		}
+		else if(key == 's')
+		{
+			m_scale *= 0.8;
+			force_redraw();
+		}
     }
 
 
@@ -250,7 +191,7 @@ int agg_main(int argc, char* argv[])
         return 1;
     }
 
-    if(app.init(512, 600, agg::window_resize))
+    if(app.init(600, 480, agg::window_resize))
     {
         return app.run();
     }
